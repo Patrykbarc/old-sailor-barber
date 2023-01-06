cf41bce0b0244bd46a017a9e42175166f633940e
Patrykbarc <108985559+Patrykbarc@users.noreply.github.com>
2023-01-06 16:18:49 +0100

    added back to home btn and fixed show more btn

--- js/script.js
+++ js/script.js
@@ -1,18 +1,32 @@
 const hamburger = document.querySelector('.hamburger')
 const closeBtn = document.querySelector('.mobile_nav__closeBtn')
 const mobileNav = document.querySelector('.mobile_nav')
+const homeBtn = document.querySelector('.home')
+const body = document.querySelector('body')
+const hero = document.querySelector('.hero')
 
 const navItems = mobileNav.querySelectorAll('li')
 
 const navToggler = () => {
 	mobileNav.classList.toggle('hidden')
+	body.classList.toggle('scroll')
 }
-
-hamburger.addEventListener('click', navToggler)
-closeBtn.addEventListener('click', navToggler)
 
 mobileNav.addEventListener('click', () => {
 	navItems.forEach(() => {
 		mobileNav.classList.add('hidden')
+		body.classList.remove('scroll')
 	})
 })
+
+const hideHome = () => {
+	if (window.scrollY >= 500 && window.innerWidth <= 992) {
+		homeBtn.style.display = 'block'
+	} else {
+		homeBtn.style.display = 'none'
+	}
+}
+
+hamburger.addEventListener('click', navToggler)
+closeBtn.addEventListener('click', navToggler)
+window.addEventListener('scroll', hideHome)
